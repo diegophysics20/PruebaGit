83c418c617c55f9f98bbb5aa762b0b524d2e402f
diegophysics20 <80854667+diegophysics20@users.noreply.github.com>
2021-09-01 12:04:04 -0500

    añadidos

--- src/GITPRUEBA.java
+++ src/GITPRUEBA.java
@@ -1,5 +1,5 @@
 public class GITPRUEBA {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo654654654");
+        System.out.println("Hola Mundo6546546545");
     }
 }
